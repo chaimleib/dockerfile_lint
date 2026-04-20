cf32b72b892d72d5008cb2e1ff304e97d1235ed7
Chaim Halbert <chaim.leib.halbert@gmail.com>
2026-04-19 17:35:00 -0700

    lint: try/catch must label the error (even unused) and label the cause on rethrow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,16 +3,27 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   {
+    files: ["**/*.{js,mjs,cjs}", "bin/dockerfile_lint"],
+    plugins: { js },
+    extends: ["js/recommended"],
     languageOptions: {
       globals: {
         ...globals.node,
       },
     },
+  },
+  {
+    files: ["**/*.js", "bin/dockerfile_lint"],
+    languageOptions: {
+      sourceType: "commonjs",
+      ecmaVersion: 6,
+    },
+  },
+  {
     rules: {
-      "no-unused-vars": ["error", { args: "none" }],
+      "no-unused-vars": ["error", { args: "none", caughtErrors: "none" }],
+      "no-var": "error",
     },
   },
 ]);
